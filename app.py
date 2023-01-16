6c8b23acf6677617ebbd742f7421d7f72bc10e39
Ansh <51117244+Anshieee@users.noreply.github.com>
2023-01-16 22:04:40 +0530

    Addition of main files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, make_response, redirect
+
+item = Query()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/bloglist')
+def contact():
+    return render_template('bloglist.html')
+
+@app.route('/introduction')
+def explore():
+    return render_template('blogs/introduction.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
